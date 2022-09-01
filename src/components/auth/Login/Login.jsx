7cf17ecb6b04a0d61715c9bedfd3b683397b1eba
TienLD <tienld@vitalify.asia>
2022-09-01 15:18:00 +0700

    Fix Bug UI

---
+++ src/components/auth/Login/Login.jsx
@@ -0,0 +1,47 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { Link } from "react-router-dom";
+import "./Login.scss";
+
+function Login() {
+  return (
+    <Form>
+      <h1>Login</h1>
+      <Form.Group className="mt-3">
+        <Form.Control
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+          // value={username}
+          // onChange={onChangeLoginForm}
+        />
+      </Form.Group>
+      <Form.Group className="mt-3">
+        <Form.Control
+          type="password"
+          placeholder="Password"
+          name="password"
+          required
+          // value={password}
+          // onChange={onChangeLoginForm}
+        />
+      </Form.Group>
+      <Button variant="success" type="submit" className="mt-4">
+        Login
+      </Button>
+      <div className="mt-4">
+        <p>
+          Don't have an account?
+          <Link to="/register">
+            <Button variant="info" size="sm" className="button-register">
+              Register
+            </Button>
+          </Link>
+        </p>
+      </div>
+    </Form>
+  );
+}
+
+export default Login;
--- src/views/Auth.jsx
+++ src/views/Auth.jsx
@@ -1,26 +1,26 @@
-import Login from "../components/auth/Login";
+import Login from "../components/auth/Login/Login";
 import Register from "../components/auth/Register";
 
 function Auth({ authRoute }) {
-	let body;
+  let body;
 
-	body = (
-		<>
-			{authRoute === "login" && <Login />}
-			{authRoute === "register" && <Register />}
-		</>
-	);
-	return (
-		<div className="landing">
-			<div className="dark-overlay">
-				<div className="landing-inner">
-					<h1>LearnIt</h1>
-					<h4>Keep track of what you are learning</h4>
-					{body}
-				</div>
-			</div>
-		</div>
-	);
+  body = (
+    <>
+      {authRoute === "login" && <Login />}
+      {authRoute === "register" && <Register />}
+    </>
+  );
+  return (
+    <div className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1>LearnIt</h1>
+          <h4>Keep track of what you are learning</h4>
+          {body}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Auth;
